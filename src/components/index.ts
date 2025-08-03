f2d2ce3ec793632b80641feb42ccebd1075f9fa7
Dong Zhou <dozhou@rubiconproject.com>
2025-08-03 00:48:49 -0700

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 // Layout components
 export { default as Layout } from './layout/Layout';
-export { default as Header } from './layout/Header'; 
+export { default as Header } from './layout/Header';
+
+// Common components
+export { default as Logo } from './common/Logo'; 
