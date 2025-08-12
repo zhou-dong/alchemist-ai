3c8781f8835a6ad2d6d01b829950b7839f741d7a
Dong Zhou <dozhou@rubiconproject.com>
2025-08-12 01:58:15 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ColorModeProvider } from './theme';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 
@@ -9,14 +7,12 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Add more routes here as your app grows */}
-          </Routes>
-        </Layout>
-      </Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here as your app grows */}
+        </Routes>
+      </Layout>
     </>
   );
 }
