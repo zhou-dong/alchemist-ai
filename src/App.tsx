c9bda9eee3265b9be91e8dd069b1438dd24a9c3a
Dong Zhou <dozhou@rubiconproject.com>
2025-08-12 02:01:30 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
+import ThemeToggleFab from './theme/ThemeToggleFab';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           {/* Add more routes here as your app grows */}
         </Routes>
       </Layout>
+      <ThemeToggleFab />
     </>
   );
 }
