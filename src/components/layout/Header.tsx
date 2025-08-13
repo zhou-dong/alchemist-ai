4925611b2e8e7070d5c614de1bcfad0abb1c9c41
Dong Zhou <dozhou@rubiconproject.com>
2025-08-12 18:12:52 -0700

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,5 @@
-import { Typography, Stack, Box, useTheme } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Typography, Stack, useTheme } from '@mui/material';
 import Logo from '../common/Logo';
-
-const HeaderContainer = styled(Box)(({ }) => ({
-  position: 'fixed',
-  top: 36,
-  left: 64,
-  zIndex: 100,
-}));
 
 export const Header = () => {
   const theme = useTheme();
@@ -15,26 +7,25 @@ export const Header = () => {
   const color = theme.palette.primary.main;
 
   return (
-    <HeaderContainer>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        gap={0.5}
-        sx={{
-          cursor: 'pointer',
-        }}
+
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      gap={0.5}
+      sx={{
+        cursor: 'pointer',
+      }}
+    >
+      <Logo width={36} height={36} color={color} />
+      <Typography
+        color={color}
+        variant="h5"
+        component="h1"
       >
-        <Logo width={36} height={36} color={color} />
-        <Typography
-          color={color}
-          variant="h5"
-          component="h1"
-        >
-          Alchemist
-        </Typography>
-      </Stack>
-    </HeaderContainer>
+        Alchemist
+      </Typography>
+    </Stack>
   );
 };
 
