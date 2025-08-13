17b2f692792c744815793c411a802d8827ce1f01
Dong Zhou <dozhou@rubiconproject.com>
2025-08-12 18:21:56 -0700

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,13 @@
 import { Typography, Stack, useTheme } from '@mui/material';
 import Logo from '../common/Logo';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
   const theme = useTheme();
-
+  const navigate = useNavigate();
   const color = theme.palette.primary.main;
 
   return (
-
     <Stack
       direction="row"
       justifyContent="center"
@@ -16,6 +16,7 @@ export const Header = () => {
       sx={{
         cursor: 'pointer',
       }}
+      onClick={() => navigate('/')}
     >
       <Logo width={36} height={36} color={color} />
       <Typography
