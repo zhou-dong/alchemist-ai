36784783859042064c6e2382a952a42b528b1d73
Dong Zhou <dozhou@rubiconproject.com>
2025-10-23 01:11:38 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Welcome from './pages/Welcome';
+import Perceptron from './pages/Perceptron';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/alchemist-ai" element={<Home />} />
           <Route path="/alchemist-ai/welcome" element={<Welcome />} />
+          <Route path="/alchemist-ai/perceptron" element={<Perceptron />} />
           {/* Add more routes here as your app grows */}
         </Routes>
       </Layout>
