52aa54e226a51d983843e5ddafb4abc95b9f3939
Dong Zhou <dozhou@rubiconproject.com>
2025-10-22 23:57:03 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/alchemist-ai" element={<Home />} />
+          <Route path="/alchemist-ai/welcome" element={<Welcome />} />
           {/* Add more routes here as your app grows */}
         </Routes>
       </Layout>
