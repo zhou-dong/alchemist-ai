5a1617fb53ef4ed3eced3f906459ff59af9a7011
Dong Zhou <dozhou@rubiconproject.com>
2026-01-11 12:57:49 -0800

    Update PlayButton.tsx

--- packages/theta-sketch/src/components/PlayButton.tsx
+++ packages/theta-sketch/src/components/PlayButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as Mouse from '@mui/icons-material/Mouse';
 import * as Done from '@mui/icons-material/Done';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
 const MouseIcon = Mouse.default as unknown as React.ElementType;
 const DoneIcon = Done.default as unknown as React.ElementType;
@@ -14,21 +14,23 @@ type PlayButtonProps = {
 };
 
 const PlayButton = ({ index, steps, disabled, onClick }: PlayButtonProps) => (
-    <Button
-        variant='contained'
-        size="large"
-        sx={{
-            position: 'fixed',
-            bottom: 100,
-            left: '50%',
-            zIndex: 1300,
-        }}
-        startIcon={index === steps.length ? <DoneIcon /> : <MouseIcon />}
-        onClick={onClick}
-        disabled={disabled}
-    >
-        {index === steps.length ? "Done" : "Next"}
-    </Button>
+    <Box sx={{
+        position: 'fixed',
+        bottom: 100,
+        left: '50%',
+        transform: 'translateX(-50%)',
+        zIndex: 1000
+    }}>
+        <Button
+            variant='contained'
+            size="large"
+            startIcon={index === steps.length ? <DoneIcon /> : <MouseIcon />}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {index === steps.length ? "Done" : "Next"}
+        </Button>
+    </Box>
 );
 
 export default PlayButton;
